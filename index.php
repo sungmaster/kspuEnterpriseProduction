53f549b31a76a9a168ef248e37a66c98eb5d02b4
Sungmaster <mihaw.wolodymyr@gmail.com>
2017-02-15 22:45:47 +0200

    no message

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL | E_STRICT);
 
 const DS    = DIRECTORY_SEPARATOR;
 const SYS   = __DIR__;
-const INC   = SYS.DS.'lib'.DS.'php'.DS;
+const INC   = '.'.DS.'lib'.DS.'php'.DS;
 
 spl_autoload_register(function ($c) {
 	$f = INC.str_replace(['\\', '_'], [DS, DS], strtolower($c)).'.php';
