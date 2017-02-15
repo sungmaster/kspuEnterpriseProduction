d10488197998b24f79fa674b0d918b0006bbd922
Sungmaster <mihaw.wolodymyr@gmail.com>
2017-02-15 23:02:24 +0200

    manual load class

--- index.php
+++ index.php
@@ -5,14 +5,9 @@ declare(strict_types = 1);
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-const DS    = DIRECTORY_SEPARATOR;
-const SYS   = __DIR__;
-const INC   = SYS.DS.'lib'.DS.'php'.DS;
-
-spl_autoload_register(function ($c) {
-	$f = INC.str_replace(['\\', '_'], [DS, DS], strtolower($c)).'.php';
-	if (file_exists($f)) include_once $f;
-});
+include_once './lib/php/controller.php';
+include_once './lib/php/model.php';
+include_once './lib/php/viewHelper.php';
 
 $controller = new Controller();
 $controller->handle();
