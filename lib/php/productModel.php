d0b5c1e8624d3d68691430563b571866940cb711
Sungmaster <mihaw.wolodymyr@gmail.com>
2017-04-04 15:39:46 +0300

    fix db connection

--- lib/php/productModel.php
+++ lib/php/productModel.php
@@ -15,7 +15,7 @@ class ProductModel
 	    if (__DIR__ == "Z:\\home\\localhost\\www\\kspu\\lib\\php")
 		    $this->db = new mysqli('localhost', 'root', '', 'production');
 	    else
-            $this->db = new mysqli('sql304.byethost10.com', 'b10_16828832', 'AlaMote_12', 'b10_16828832_production');
+            $this->db = new mysqli('localhost', 'phpuser', 'QDDWlc9m9B4XTJMS', 'production');
 		$this->db->set_charset("utf8");
 	}
 	public function __destruct(){
